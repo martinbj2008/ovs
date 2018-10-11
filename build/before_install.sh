10e8736221f3d7fb1ac4217ea5bb7402b392fb98
sunwenjie <sunwenjie@didiglobal.com>
2018-10-11 22:20:57 +0800

    Update before_install.sh

--- build/before_install.sh
+++ build/before_install.sh
@@ -1,3 +1,8 @@
+rm -f /tmp/ovs-dpdk.conf
+if [ -f /etc/openvswitch/ovs-dpdk.conf ];then
+    mv /etc/openvswitch/ovs-dpdk.conf /tmp/
+fi
+
 mkdir -p /root/ovs-last
 if [ -f /usr/sbin/ovs-vswitchd ] && [ -f /usr/sbin/ovsdb-server ];then
 /usr/bin/cp -f /usr/sbin/ovs-vswitchd /usr/sbin/ovsdb-server /root/ovs-last
