martinbj2008/ovs

commit cc4ab03165e3a0597eb398c3ee7342482075361a

[SDNETWORK-1140]dpcls: add mod flow hardware flags
ex:
    ovs-appctl dpctl/mod-flow "flags:skip_hw,in_port(3),eth(),"\
    "eth_type(0x0800),ipv4(dst=10.97.234.222)" \
    "meter(10000),counter(1),set(eth(src=00:00:10:10:10:01,dst=00:00:00:00:00:40)),"\
    "set(ipv4(dst=10.10.10.222)),recirc(4278190091)"

    This command will auto delele offload flows,  and all packets match this
    flow will exec by software.

Signed-off-by: dujie <dujie@didiglobal.com>

lib/dpif-netdev.c
@@ -3541,8 +3541,10 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
         }
     } else {
         if (put->flags & DPIF_FP_MODIFY) {
+            uint32_t old_flags = netdev_flow->flow_flags;
             struct dp_netdev_actions *new_actions;
             struct dp_netdev_actions *old_actions;
+            unsigned long long n;
 
             new_actions = dp_netdev_actions_create(put->actions,
                                                    put->actions_len);
@@ -3550,8 +3552,26 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
             old_actions = dp_netdev_flow_get_actions(netdev_flow);
             ovsrcu_set(&netdev_flow->actions, new_actions);
 
-            queue_netdev_flow_put(pmd, netdev_flow, match,
+            if (put->para.flow_flags == DPCLS_RULE_FLAGS_SKIP_HW) {
+                netdev_flow->flow_flags = put->para.flow_flags;
+                if (old_flags != DPCLS_RULE_FLAGS_SKIP_HW) {
+                    queue_netdev_flow_del(pmd, netdev_flow);
+
+                    atomic_read_relaxed(&netdev_flow->old_stats.packet_count, &n);
+                    non_atomic_ullong_add(&netdev_flow->stats.packet_count, n);
+                    atomic_read_relaxed(&netdev_flow->old_stats.byte_count, &n);
+                    non_atomic_ullong_add(&netdev_flow->stats.byte_count, n);
+
+                    atomic_store_relaxed(&netdev_flow->old_stats.packet_count, 0);
+                    atomic_store_relaxed(&netdev_flow->old_stats.byte_count, 0);
+                }
+            } else {
+                netdev_flow->flow_flags = put->para.flow_flags;
+                atomic_store_relaxed(&netdev_flow->old_stats.packet_count, 0);
+                atomic_store_relaxed(&netdev_flow->old_stats.byte_count, 0);
+                queue_netdev_flow_put(pmd, netdev_flow, match,
                                   put->actions, put->actions_len);
+            }
 
             if (stats) {
                 get_dpif_flow_stats(netdev_flow, stats);

commit 144146bc74da10880514d6ed4fbeb39a8c51d6fe

[SDNETWORK-1146]: don't delete the flow when mod-flow
Fixes: cc4ab03165e3 ("[SDNETWORK-1140]dpcls: add mod flow hardware flags")
Cc: dujie <dujie@didiglobal.com>

Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>

lib/dpif-netdev.c
@@ -3555,7 +3555,6 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
             if (put->para.flow_flags == DPCLS_RULE_FLAGS_SKIP_HW) {
                 netdev_flow->flow_flags = put->para.flow_flags;
                 if (old_flags != DPCLS_RULE_FLAGS_SKIP_HW) {
-                    queue_netdev_flow_del(pmd, netdev_flow);
 
                     atomic_read_relaxed(&netdev_flow->old_stats.packet_count, &n);
                     non_atomic_ullong_add(&netdev_flow->stats.packet_count, n);