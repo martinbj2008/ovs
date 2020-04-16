martinbj2008/ovs

commit cc4ab03165e3a0597eb398c3ee7342482075361a

[SDNETWORK-1140]dpcls: add mod flow hardware flags
ex:
    ovs-appctl dpctl/mod-flow "flags:skip_hw,in_port(3),eth(),"\
    "eth_type(0x0800),ipv4(dst=10.97.234.222)" \
    "meter(10000),counter(1),set(eth(src=00:00:10:10:10:01,dst=00:00:00:00:00:40)),"\
    "set(ipv4(dst=10.10.10.222)),recirc(4278190091)"

    This command will auto delele offload flows,  and all packets match this
    flow will exec by software.

Signed-off-by: dujie <dujie@didiglobal.com>

lib/dpif-netdev.c
@@ -3541,8 +3541,10 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
         }
     } else {
         if (put->flags & DPIF_FP_MODIFY) {
+            uint32_t old_flags = netdev_flow->flow_flags;
             struct dp_netdev_actions *new_actions;
             struct dp_netdev_actions *old_actions;
+            unsigned long long n;
 
             new_actions = dp_netdev_actions_create(put->actions,
                                                    put->actions_len);
@@ -3550,8 +3552,26 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
             old_actions = dp_netdev_flow_get_actions(netdev_flow);
             ovsrcu_set(&netdev_flow->actions, new_actions);
 
-            queue_netdev_flow_put(pmd, netdev_flow, match,
+            if (put->para.flow_flags == DPCLS_RULE_FLAGS_SKIP_HW) {
+                netdev_flow->flow_flags = put->para.flow_flags;
+                if (old_flags != DPCLS_RULE_FLAGS_SKIP_HW) {
+                    queue_netdev_flow_del(pmd, netdev_flow);
+
+                    atomic_read_relaxed(&netdev_flow->old_stats.packet_count, &n);
+                    non_atomic_ullong_add(&netdev_flow->stats.packet_count, n);
+                    atomic_read_relaxed(&netdev_flow->old_stats.byte_count, &n);
+                    non_atomic_ullong_add(&netdev_flow->stats.byte_count, n);
+
+                    atomic_store_relaxed(&netdev_flow->old_stats.packet_count, 0);
+                    atomic_store_relaxed(&netdev_flow->old_stats.byte_count, 0);
+                }
+            } else {
+                netdev_flow->flow_flags = put->para.flow_flags;
+                atomic_store_relaxed(&netdev_flow->old_stats.packet_count, 0);
+                atomic_store_relaxed(&netdev_flow->old_stats.byte_count, 0);
+                queue_netdev_flow_put(pmd, netdev_flow, match,
                                   put->actions, put->actions_len);
+            }
 
             if (stats) {
                 get_dpif_flow_stats(netdev_flow, stats);

commit 4f94fb28bae5de7e66ef63d0e35272afc4582b47

[SDNETWORK-1146]: dpcls: add check for no-offload flow
Fixes: cc4ab03165e3 ("[SDNETWORK-1140]dpcls: add mod flow hardware flags")
Cc: dujie <dujie@didiglobal.com>

Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>

lib/dpif-netdev.c
@@ -3518,6 +3518,32 @@ dp_netdev_flow_add(struct dp_netdev_pmd_thread *pmd,
     return flow;
 }
 
+static void
+try_queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
+                          struct dp_netdev_flow *netdev_flow)
+{
+    odp_port_t odp_port = netdev_flow->flow.in_port.odp_port;
+    struct dp_netdev *dp = pmd->dp;
+    struct netdev_flow_dump dump;
+    struct dp_netdev_port *port;
+    struct dpif_flow_stats stats;
+    bool offload;
+
+    ovs_mutex_lock(&dp->port_mutex);
+    port = dp_netdev_lookup_port(dp, odp_port);
+    ovs_mutex_unlock(&dp->port_mutex);
+
+    if (port == NULL)
+        return;
+
+    dump.netdev = port->netdev;
+    offload = netdev_flow_dump_next(&dump, NULL, NULL, &stats, NULL,
+                                    CONST_CAST(ovs_u128 *, &netdev_flow->mega_ufid),
+                                    NULL, NULL);
+    if (offload)
+        queue_netdev_flow_del(pmd, netdev_flow);
+}
+
 static int
 flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
                 struct netdev_flow_key *key,
@@ -3567,7 +3593,8 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
             if (put->para.flow_flags == DPCLS_RULE_FLAGS_SKIP_HW) {
                 netdev_flow->flow_flags = put->para.flow_flags;
                 if (old_flags != DPCLS_RULE_FLAGS_SKIP_HW) {
-                    queue_netdev_flow_del(pmd, netdev_flow);
+
+                    try_queue_netdev_flow_del(pmd, netdev_flow);
 
                     atomic_read_relaxed(&netdev_flow->old_stats.packet_count, &n);
                     non_atomic_ullong_add(&netdev_flow->stats.packet_count, n);