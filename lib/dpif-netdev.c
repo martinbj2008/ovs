martinbj2008/ovs

commit 4f94fb28bae5de7e66ef63d0e35272afc4582b47

[SDNETWORK-1146]: dpcls: add check for no-offload flow
Fixes: cc4ab03165e3 ("[SDNETWORK-1140]dpcls: add mod flow hardware flags")
Cc: dujie <dujie@didiglobal.com>

Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>

lib/dpif-netdev.c
@@ -3518,6 +3518,32 @@ dp_netdev_flow_add(struct dp_netdev_pmd_thread *pmd,
     return flow;
 }
 
+static void
+try_queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
+                          struct dp_netdev_flow *netdev_flow)
+{
+    odp_port_t odp_port = netdev_flow->flow.in_port.odp_port;
+    struct dp_netdev *dp = pmd->dp;
+    struct netdev_flow_dump dump;
+    struct dp_netdev_port *port;
+    struct dpif_flow_stats stats;
+    bool offload;
+
+    ovs_mutex_lock(&dp->port_mutex);
+    port = dp_netdev_lookup_port(dp, odp_port);
+    ovs_mutex_unlock(&dp->port_mutex);
+
+    if (port == NULL)
+        return;
+
+    dump.netdev = port->netdev;
+    offload = netdev_flow_dump_next(&dump, NULL, NULL, &stats, NULL,
+                                    CONST_CAST(ovs_u128 *, &netdev_flow->mega_ufid),
+                                    NULL, NULL);
+    if (offload)
+        queue_netdev_flow_del(pmd, netdev_flow);
+}
+
 static int
 flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
                 struct netdev_flow_key *key,
@@ -3567,7 +3593,8 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
             if (put->para.flow_flags == DPCLS_RULE_FLAGS_SKIP_HW) {
                 netdev_flow->flow_flags = put->para.flow_flags;
                 if (old_flags != DPCLS_RULE_FLAGS_SKIP_HW) {
-                    queue_netdev_flow_del(pmd, netdev_flow);
+
+                    try_queue_netdev_flow_del(pmd, netdev_flow);
 
                     atomic_read_relaxed(&netdev_flow->old_stats.packet_count, &n);
                     non_atomic_ullong_add(&netdev_flow->stats.packet_count, n);

commit ab58222d3016aad9ed374191f50e70b048ccb61a

[SDNETWORK-1168]: dpctl: map the vxlan_sys_4789 to vxlan-offload
Fixes: 4f94fb28bae5 ("[SDNETWORK-1146]: dpcls: add check for no-offload flow")
Cc: Tonghao Zhang <xiangxia.m.yue@gmail.com>

Signed-off-by: Tonghao Zhang <xiangxia.m.yue@gmail.com>

lib/dpif-netdev.c
@@ -3528,6 +3528,7 @@ try_queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
     struct dp_netdev_port *port;
     struct dpif_flow_stats stats;
     bool offload;
+    int ret;
 
     ovs_mutex_lock(&dp->port_mutex);
     port = dp_netdev_lookup_port(dp, odp_port);
@@ -3536,6 +3537,14 @@ try_queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
     if (port == NULL)
         return;
 
+    if (!strcmp(port->netdev->name, "vxlan_sys_4789")) {
+        ovs_mutex_lock(&dp->port_mutex);
+        ret = get_port_by_name(dp, "vxlan-offload", &port);
+        ovs_mutex_unlock(&dp->port_mutex);
+
+        if(ret)
+            return;
+    }
     dump.netdev = port->netdev;
     offload = netdev_flow_dump_next(&dump, NULL, NULL, &stats, NULL,
                                     CONST_CAST(ovs_u128 *, &netdev_flow->mega_ufid),

lib/netdev-offload-dpdk.c
@@ -125,7 +125,9 @@ struct output_port_map port_map[] = {
         .pdev=NULL,
     },
     [VXLAN_OFFLOAD]={
-        .name="vxlan-offload",
+        .name="vxlan-offload", /* NOTE: If you would like to change that name, pls
+                                * change the try_queue_netdev_flow_del function.
+                                */
         .port_id = 0,
         .pdev=NULL,
     },